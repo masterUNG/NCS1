754df5004d4934576b4ae8f7bf027cfe95a97290
masterUNG <phrombutr@gmail.com>
2016-04-14 16:29:11 +0700

    Authentication Finish

--- app/src/main/java/rmutr/boonsathain/wanwisa/ncs1/MainActivity.java
+++ app/src/main/java/rmutr/boonsathain/wanwisa/ncs1/MainActivity.java
@@ -1,5 +1,6 @@
 package rmutr.boonsathain.wanwisa.ncs1;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -122,9 +124,46 @@ public class MainActivity extends AppCompatActivity {
 
         } else {
             //No Space
+            checkUser();
 
         }
 
     }   // clickLogin
+
+    private void checkUser() {
+
+        try {
+
+            SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                    MODE_PRIVATE, null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM userTABLE WHERE User = " + "'" + userString + "'", null);
+            cursor.moveToFirst();
+            String[] resultStrings = new String[cursor.getColumnCount()];
+            for (int i=0;i<cursor.getColumnCount();i++) {
+                resultStrings[i] = cursor.getString(i);
+            }   // for
+            cursor.close();
+
+            //Check Password
+            if (passwordString.equals(resultStrings[2])) {
+                //Password True
+                Toast.makeText(this, "ยินดีต้อนรับ " + resultStrings[4] + " สู่ระบบของเรา",
+                        Toast.LENGTH_SHORT).show();
+
+            } else {
+                //Password False
+                MyAlert myAlert = new MyAlert();
+                myAlert.myDialog(this, "Password False",
+                        "กรุณาพิมพ์ใหม่ Password ผิด");
+
+            }
+
+        } catch (Exception e) {
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "ไม่มี User นี่",
+                    "ไม่มี " + userString + " ในฐานข้อมูล ของเรา");
+        }   // try
+
+    }   // checkUser
 
 }   // Main Class
