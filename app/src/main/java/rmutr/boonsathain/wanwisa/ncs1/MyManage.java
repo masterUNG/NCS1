314a7c1aab8d1a4bf73805c0a846e88db65f81d0
masterUNG <phrombutr@gmail.com>
2016-04-15 17:10:21 +0700

    Finish 15April16

--- app/src/main/java/rmutr/boonsathain/wanwisa/ncs1/MyManage.java
+++ app/src/main/java/rmutr/boonsathain/wanwisa/ncs1/MyManage.java
@@ -21,12 +21,64 @@ public class MyManage {
     public static final String column_Name = "Name";
     public static final String column_Surname = "Surname";
 
+    public static final String boat_table = "boatTABLE";
+    public static final String column_data2 = "data2";
+    public static final String column_data3 = "data3";
+    public static final String column_data4 = "data4";
+    public static final String column_data5 = "data5";
+    public static final String column_data6 = "data6";
+    public static final String column_data7 = "data7";
+    public static final String column_data8 = "data8";
+    public static final String column_data9 = "data9";
+    public static final String column_data10 = "data10";
+    public static final String column_data11 = "data11";
+    public static final String column_data12 = "data12";
+    public static final String column_data13 = "data13";
+    public static final String column_data14 = "data14";
+    public static final String column_data15 = "data15";
+    public static final String column_data16 = "data16";
+
     public MyManage(Context context) {
 
         myOpenHelper = new MyOpenHelper(context);
         sqLiteDatabase = myOpenHelper.getWritableDatabase();
 
     }   //Constructor
+
+    public long addNewBoat(String strdata2,
+                           String strdata3,
+                           String strdata4,
+                           String strdata5,
+                           String strdata6,
+                           String strdata7,
+                           String strdata8,
+                           String strdata9,
+                           String strdata10,
+                           String strdata11,
+                           String strdata12,
+                           String strdata13,
+                           String strdata14,
+                           String strdata15) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_data2, strdata2);
+        contentValues.put(column_data3, strdata3);
+        contentValues.put(column_data4, strdata4);
+        contentValues.put(column_data5, strdata5);
+        contentValues.put(column_data6, strdata6);
+        contentValues.put(column_data7, strdata7);
+        contentValues.put(column_data8, strdata8);
+        contentValues.put(column_data9, strdata9);
+        contentValues.put(column_data10, strdata10);
+        contentValues.put(column_data11, strdata11);
+        contentValues.put(column_data12, strdata12);
+        contentValues.put(column_data13, strdata13);
+        contentValues.put(column_data14, strdata14);
+        contentValues.put(column_data15, strdata15);
+
+
+        return sqLiteDatabase.insert(boat_table, null, contentValues);
+    }
 
     public long addNewUser(String strUser,
                            String strPassword,
